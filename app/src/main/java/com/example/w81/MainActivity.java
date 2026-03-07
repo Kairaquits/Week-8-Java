5beb929525b86d1af56f16e91ffa0456e79276b0
Kani <kranjani3100@gmail.com>
2026-03-07 22:16:07 +0200

    Base done, only UI edits left if needed

---
+++ app/src/main/java/com/example/w81/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.w81;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText field1;
+    private EditText field2;
+    private EditText field3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        setContentView(R.layout.activity_main);
+        field1 = findViewById(R.id.textField1);
+        field2 = findViewById(R.id.textField2);
+        field3 = findViewById(R.id.Result);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+
+    public void Add(View view){
+        double n1 = Integer.parseInt(field1.getText().toString());
+        double n2 = Integer.parseInt(field2.getText().toString());
+        double result = n1+n2;
+
+        field3.setText(String.valueOf(result));
+
+    }
+
+    public void Subtract(View view){
+        double n1 = Integer.parseInt(field1.getText().toString());
+        double n2 = Integer.parseInt(field2.getText().toString());
+        double result = n1-n2;
+
+        field3.setText(String.valueOf(result));
+
+    }
+
+    public void Multiply(View view){
+        double n1 = Integer.parseInt(field1.getText().toString());
+        double n2 = Integer.parseInt(field2.getText().toString());
+        double result = n1*n2;
+
+        field3.setText(String.format("%.5f", result));
+
+    }
+
+    public void Division(View view){
+        double n1 = Integer.parseInt(field1.getText().toString());
+        double n2 = Integer.parseInt(field2.getText().toString());
+
+        if (n2==0){
+            field3.setText("Cannot divide by 0");
+            return;
+        }
+        double result = n1/n2;
+        field3.setText(String.format("%.5f", result));
+    }
+}
